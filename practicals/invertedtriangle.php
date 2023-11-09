e54185e6b608460880770ff08c55173c9987dea2
“Lahotiharsh” <lahotiharsh07@gmail.com>
2023-11-09 16:54:42 +0530

    Update inverted triangle pattern for readability and maintainability

--- practicals/invertedtriangle.php
+++ practicals/invertedtriangle.php
@@ -1,15 +1,24 @@
 <?php
-// Number of rows in the inverted triangle
-$rows = 5;
+// PHP program to print an inverted triangle pattern
 
-// Outer loop for number of rows
-for ($i = $rows; $i >= 1; $i--) {
-    // Inner loop for printing stars
-    for ($j = 1; $j <= $i; $j++) {
-        echo "*";
+// Function to print an inverted triangle pattern with a specified number of rows
+function printInvertedTriangle($rowCount) {
+    // Outer loop for each row
+    for ($row = $rowCount; $row >= 1; $row--) {
+        // Inner loop for each column in the current row
+        for ($column = 1; $column <= $row; $column++) {
+            // Print a star for each column
+            echo "*";
+        }
+        // Move to the next line after each row
+        echo "\n";
     }
-    // Move to the next line after each row
-    echo "\n";
 }
+
+// Number of rows in the inverted triangle pattern
+$invertedTriangleRows = 5;
+
+// Call the function to print the inverted triangle pattern
+printInvertedTriangle($invertedTriangleRows);
 ?>
 
