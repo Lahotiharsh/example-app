3cd5311a97d1e83bfcd18ab08567b160be4c629c
“Lahotiharsh” <lahotiharsh07@gmail.com>
2023-11-09 16:35:22 +0530

    Update hello world program for readability and maintainability

--- practicals/helloworld.php
+++ practicals/helloworld.php
@@ -1,4 +1,10 @@
 <?php
-echo "Hello, World!";
+// PHP program to print "Hello, World!"
+
+// Define a variable to store the greeting message
+$greetingMessage = "Hello, World!";
+
+// Print the greeting message
+echo $greetingMessage;
 ?>
 
